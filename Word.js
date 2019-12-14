5b6b62b359762c2ae555daffd31c0da07c43c751
Cefalu Wave <luke.evan.graham@gmail.com>
2019-12-13 22:57:29 -0800

    checker works but correct value doesn't display

--- Word.js
+++ Word.js
@@ -1,9 +1,36 @@
 var Letter = require("./Letter")
 
-let h = new Letter("h")
+// let h = new Letter("h")
 
-console.log(h + '')
+// console.log(h + '')
 
-h.checker("a")
+// h.checker("h")
 
-console.log(h + '')
+// console.log(h + '')
+
+function Word(answer) {
+    this.wordObject = [];
+
+    for (let i = 0; i < answer.length; i++) {
+        var letter = new Letter(answer[i]);
+        this.wordObject.push(letter);
+    }
+    
+    this.currentDisplay = this.wordObject + ''
+
+    this.testGuess = function(guess) {
+        for (let i = 0; i < this.wordObject.length; i++) {
+            this.wordObject[i].checker(guess)
+            
+        }
+        // Letter.checker(guess)
+    }
+    
+    // console.log("LOOK HERE: ", this);
+}
+
+test = new Word("test");
+
+test.testGuess("t")
+
+console.log("BIG TIME: ", test)
