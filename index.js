eb8ff75e8c01cff1caffd05d9f4ef55358c3624d
Cefalu Wave <luke.evan.graham@gmail.com>
2019-12-22 11:58:19 -0800

    word guessing happens

--- index.js
+++ index.js
@@ -2,24 +2,38 @@ let inquirer = require("inquirer");
 let Word = require("./Word")
 let possibleWords = require("./PossibleWords")
 
-console.log("Hello friend!")
-
-// hi = new Word("hi")
+function askForLetter() {
+    inquirer.prompt([
+        {
+            name: "enteredLetter",
+    message: "Guess a letter"
+        }
+    ]).then(function(answers) {
+        word.testGuess(answers.enteredLetter)
+        console.log(word + '\n')
+        askForLetter()
+    })
+}
 
 var randomWord = possibleWords[Math.floor(Math.random() * possibleWords.length)]
 
-
-
 let word = new Word(randomWord)
 
-console.log(word + '')
+console.log(word + '\n')
 
-word.testGuess("t")
 
-console.log(word + '')
+askForLetter()
 
-word.testGuess("e")
 
-console.log(word + '')
-console.log(possibleWords)
-console.log(randomWord)
+
+
+
+// word.testGuess("t")
+
+// console.log(word + '')
+
+// word.testGuess("e")
+
+// console.log(word + '')
+// console.log(possibleWords)
+// console.log(randomWord)
